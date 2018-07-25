83043d430fb99f5a941cf2b5da8dc779b6dc22bd
blaws006 <blaws006>
2018-07-25 19:26:37 -0400

    Making sure that people see the notice...

--- assets/javascript/newapp.js
+++ assets/javascript/newapp.js
@@ -1,3 +1,14 @@
+// Needs a start screen 
+// Right answer produces a right answer screen
+// Wrong answer produces a wrong answer screen
+// Both right answer and wrong answer screens produce right anwser w/ gif
+// Screen goes to next question after question is answered or the timer runs out
+// Need a setInterval function to create the timer - wrong answer if timer runs out
+// Needs a screen that records number of right and wrong answers - Play Again button resets the game
+
+// NEW Challenges
+// Let's write this in ES6 and convert everything to the modular pattern!
+
 // Trivia Controller
 const triviaController = (() =>{
 
