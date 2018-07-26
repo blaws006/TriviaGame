544aa6ba20bbcda81c5c6fe951aa881fbd8ada28
blaws006 <blaws006>
2018-07-25 21:55:11 -0400

    Finally got my initial event listeners set up

--- assets/javascript/newapp.js
+++ assets/javascript/newapp.js
@@ -10,17 +10,55 @@
 // Let's write this in ES6 and convert everything to the modular pattern!
 
 // Trivia Controller
-const triviaController = (() =>{
-
+const triviaController = (() => {
+  // Map each answer
+  // Hold right or wrong answer data
 })();
- 
- // UI Controller
- const UIController = (() =>{
 
- })();
+// UI Controller
+const UIController = (() => {
+  const DOMstrings = {
+    startButton: '#press-start',
+    answerOne: '.answer-one',
+    answerTwo: '.answer-two',
+    answerThree: '.answer-three',
+    answerFour: '.answer-four',
+    img: '.image',
+    time: '.time',
+    gameSection: '#gameSectionContainer'
+  }
+
+  return {
+    getDOMstrings: () => {
+      return DOMstrings;
+    },
+    displayQuestions: () => {
+      const startPage = document.querySelector(DOMstrings.gameSection)
+      const triviaPage = startPage.innerHTML = `<div class="page-two"><div class="text-center"><h4>Time Remaining: <span class="time">0:00</span></h4><hr></div><div><div class="text-center" id="question"><p></p></div></div><div class="text-center"><div class="row"><div class=" answer col-md-12"></div></div><div class="row"><div class="answer col-md-12"></div></div><div class="row"><div class="answer col-md-12"></div></div><div class="row"><div class="answer col-md-12"></div></div></div></div>`;
+      return triviaPage
+    }
+  }
+  // Display trivia pages
+  // Display right/wrong answer page
+  // Dislpay final tally
+  // Display timer and reset after each question
+})();
 
 // App Controller
 
-const appController = ((triviaCtrl, UICtrl) =>{
+const appController = ((triviaCtrl, UICtrl) => {
 
+  const setupEventListeners = () => {
+    const DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.startButton).addEventListener('click', () => {
+      UICtrl.displayQuestions();
+    });
+  }
+  return {
+    init: () => {
+      setupEventListeners();
+    }
+  }
 })(triviaController, UIController);
+
+appController.init();
